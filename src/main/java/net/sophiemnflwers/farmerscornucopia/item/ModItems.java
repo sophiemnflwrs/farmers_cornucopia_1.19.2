8e938d0a4ec05eaea4ac2a04e606c7ccbd20e11d
sophiemnflwrs <sophietirado@yahoo.com>
2024-03-22 22:27:44 -0400

    mod items

---
+++ src/main/java/net/sophiemnflwers/farmerscornucopia/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.sophiemnflwers.farmerscornucopia.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.sophiemnflwers.farmerscornucopia.FarmersCornucopia;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FarmersCornucopia.MOD_ID);
+
+    public static final RegistryObject<Item> GARLICHEAD = ITEMS.register("garlichead",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
